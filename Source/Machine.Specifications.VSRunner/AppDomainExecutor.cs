8898e9c535585f474db0de7f5af19c8f8787b376
Eugene Duvenage <eugene@eugeneduvenage.com>
2013-04-14 16:51:35 +0200

    added dynamic conditional to check of mspec >= 0.5.12 to fix IAssemblyContext running once for each test

--- Source/Machine.Specifications.VSRunner/AppDomainExecutor.cs
+++ Source/Machine.Specifications.VSRunner/AppDomainExecutor.cs
@@ -2,6 +2,8 @@
 using Machine.Specifications.Runner.Impl;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -29,12 +31,34 @@ namespace Machine.Specifications.VSRunner
         public void RunTestsInAssembly(string pathToAssembly, IEnumerable<string> specsToRun, ISpecificationRunListener specificationRunListener)
         {
             DefaultRunner mspecRunner = null;
+            dynamic dynMSpeccRunner = null;
             Assembly assemblyToRun = null;
+            bool canIndicateStartAndEnd = false;
+
+            // determine the mspec version, if its greater or equal to 0.5.12 we can call the start and endrun methods in mspec
+            string pathToMSpec = Path.Combine(Path.GetDirectoryName(pathToAssembly), "Machine.Specifications.dll");
+            if (File.Exists(pathToMSpec))
+            {
+                FileVersionInfo fileInfo = FileVersionInfo.GetVersionInfo(pathToMSpec);
+                if (fileInfo.FileMinorPart > 5)
+                {
+                    canIndicateStartAndEnd = true;
+                }
+                else
+                    if (fileInfo.FileMinorPart == 5 && fileInfo.FileBuildPart >= 12)
+                    {
+                        canIndicateStartAndEnd = true;
+                    }
+            }
             try
             {
                 assemblyToRun = Assembly.LoadFrom(pathToAssembly);
                 mspecRunner = new DefaultRunner(specificationRunListener, RunOptions.Default);
-                mspecRunner.StartRun(assemblyToRun);
+                dynMSpeccRunner = mspecRunner;
+                if (canIndicateStartAndEnd)
+                {
+                    dynMSpeccRunner.StartRun(assemblyToRun);
+                }
                 foreach (string spec in specsToRun)
                 {
                     // get the spec type
@@ -56,8 +80,12 @@ namespace Machine.Specifications.VSRunner
             {
                 if (mspecRunner != null && assemblyToRun != null)
                 {
-                    mspecRunner.EndRun(assemblyToRun);
+                    if (canIndicateStartAndEnd)
+                    {
+                        dynMSpeccRunner.EndRun(assemblyToRun);
+                    }
                     mspecRunner = null;
+                    dynMSpeccRunner = null;
                     assemblyToRun = null;
                 }
             }
