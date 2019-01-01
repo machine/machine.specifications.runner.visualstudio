62be4df2cd060d4908d543d34a4d7fc1f519358b
Robert Coltheart <robert.coltheart@gmail.com>
2019-01-01 20:27:44 +0800

    add tests

---
+++ Source/Machine.VSTestAdapter.Specs/Execution/With_MultipleSpecExecutionSetup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Machine.Fakes;
+using Machine.Specifications;
+using Machine.VSTestAdapter.Configuration;
+using Machine.VSTestAdapter.Execution;
+using Machine.VSTestAdapter.Helpers;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+
+namespace Machine.VSTestAdapter.Specs.Execution
+{
+    public abstract class With_MultipleSpecExecutionSetup : WithFakes
+    {
+        protected static ISpecificationExecutor Executor;
+        protected static string AssemblyPath;
+        protected static VisualStudioTestIdentifier[] SpecificationsToRun;
+
+        Establish context = () => 
+        {
+            Executor = new SpecificationExecutor();
+            AssemblyPath = Path.Combine(Helper.GetTestDirectory(), "SampleSpecs.dll");
+        };
+
+        Because of = () => 
+            Executor.RunAssemblySpecifications(AssemblyPath, SpecificationsToRun, The<Settings>(), new Uri("bla://executor"), The<IFrameworkHandle>());
+    }
+}
