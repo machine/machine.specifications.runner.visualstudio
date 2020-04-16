b12ac38ae4368014f687cd4cb0eff611adeca952
Michael Blake <mikeblake@hotmail.com>
2020-04-16 15:49:08 +0100

    Add IFrameworkLogger

---
+++ src/Machine.Specifications.Runner.VisualStudio/Execution/IFrameworkLogger.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+
+namespace Machine.VSTestAdapter.Execution
+{
+    public interface IFrameworkLogger
+    {
+        void SendMessage(TestMessageLevel level, string message);
+    }
+}
